a7f0b44652c26eae20294d8bcacb39ef0dd44586
Jose Luis <luismoyarm@gmail.com>
2023-07-21 14:56:24 -0600

    first commit

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import {useState, useContext} from 'react';
+import {TaskContext} from '../context/TaskContext'
+
+function TaskForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const {createTask} = useContext(TaskContext)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask({
+      title, 
+      description
+    });
+    setTitle('')
+    setDescription('')
+  }
+
+  return (
+    <div className='max-w-md mx-auto'>
+      <form onSubmit={handleSubmit} className='bg-slate-800 p-10 mb-4 '>
+        <h1 className='text-2xl font-bold text-white mb-3'>Crea tu tarea</h1>
+        <input
+          placeholder="Título de la tarea"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          className='bg-slate-300 px-3 w-full mb-2'
+          autoFocus
+        />
+        <textarea placeholder="Escribe la descripcion de la tarea" onChange={e => setDescription(e.target.value)}
+        className='bg-slate-300 px-3 w-full mb-2'
+        value={description}
+        ></textarea>
+        <button type="submit"
+        className='bg-indigo-500 px-3 py-1 text-white'
+        >Guardar</button>
+      </form>
+    </div>
+  );
+}
+
+TaskForm.propTypes = {
+  createTask: PropTypes.func.isRequired,
+};
+
+export default TaskForm;
